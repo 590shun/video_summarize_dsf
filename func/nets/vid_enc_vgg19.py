3525db857abac5d2820ca1454dd45af5294506b1
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-04-22 17:32:28 +0900

    Update vid_enc_vgg19.py

--- func/nets/vid_enc_vgg19.py
+++ func/nets/vid_enc_vgg19.py
@@ -1,16 +1,23 @@
-import chainer.functions as F
-from chainer import Chain
+#-*- coding:utf-8 -*-
 
+#モジュールのインポート#
+import torch.nn.functional as F
+import torch.nn as nn
 
-class Model(Chain):
-
+#クラス定義#
+class Model(nn.Module):
     def __init__(self):
-        self.b_size = {'video': 5}
-
+        self.batch_size = {'video':5}
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(4096, 1000)
+        self.fc2 = nn.Linear(1000, 300)
+    
     def __call__(self, x_seg):
-        '''
-        input: np.array(5xN, 4096)
-        '''
-        b_size = self.b_size['video']
-        out_size = x_seg.shape[1]
-        return F.sum(F.reshape(x_seg, (x_seg.shape[0] / b_size, b_size, out_size)), axis=1) / b_size
+        # 入力:np.array(5*N, 4096)
+        batch_size = self.batch_size['video']
+        with cuda.get_device(x_seg.data):
+            y0 = F.tanh(self.fc1(x_seg))
+            # print(y0.shape)
+            y1 = F.tanh(self.fc2(y0))
+            h = F.reshape(y1, (y1.shape[0] / batch_size, batch_size, 300))
+            return F.sum(h, axis=1) / batch_size
