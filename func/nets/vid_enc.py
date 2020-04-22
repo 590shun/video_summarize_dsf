422cae2b120703662810f6590b6ed6c2720a42f3
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-04-22 18:04:31 +0900

    Update vid_enc.py

--- func/nets/vid_enc.py
+++ func/nets/vid_enc.py
@@ -1,31 +1,26 @@
-import chainer.links as L
-import chainer.functions as F
-from chainer import cuda, serializers, Chain, Variable
-from os.path import dirname
-import json
+#-*- coding:utf-8 -*-
+
+#モジュールのインポート#
+import sys, os
 import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from os.path import dirname
 from numpy.random import RandomState
+import json
 
-import sys
-
-
-class Model(Chain):
-
-    def __init__(self, b_size={'video': 5}):
-        self.b_size = b_size
-
-        super(Model, self).__init__(
-            fc_v1=L.Linear(4096, 1000),
-            fc_v2=L.Linear(1000, 300),
-        )
+#クラス定義#
+class Model(nn.Module):
+    def __init__(self, batch_size={'video':5}):
+        self.batch_size = batch_size
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(4096, 1000)
+        self.fc2 = nn.Liner(1000, 300)
 
     def __call__(self, x_seg):
-        '''
-        input: np.array(5xN, 4096)
-        '''
-        b_size = self.b_size['video']
+        batch_size = self.batch_size['video']
         with cuda.get_device(x_seg.data):
-            y0 = F.tanh(self.fc_v1(x_seg))
-            y1 = F.tanh(self.fc_v2(y0))
-            h = F.reshape(y1, (y1.shape[0] / b_size, b_size, 300))
-            return F.sum(h, axis=1) / b_size
+            y0 = F.tanh(self.fc1(x_seg))
+            y1 = F.tanh(self.fc2(y0))
+            h = F.reshape(y1, (y1.shape[0] / batch_size, batch_size, 300))
+            return F.sum(h, axis=1) / batch_size
