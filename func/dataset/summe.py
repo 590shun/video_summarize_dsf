43c7398af169799cea6a9694321ca80a19bcd22e
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-04-22 15:42:21 +0900

    Update summe.py

--- func/dataset/summe.py
+++ func/dataset/summe.py
@@ -1,28 +1,37 @@
-import json
+#-*- coding:utf-8 -*-
+
+##SumMeデータセットのロード##
+
+#モジュールのインポート#
 import numpy as np
+import json
 
-datasetRoot = 'data/summe/'
+#データセットが存在するフォルダの指定#
+data_root = './data/summe'
 
-
-class SUMME():
-
+#クラスの定義#
+class summe():
     def __init__(self, video_id, feat_type='vgg'):
-
-        dataset = json.load(open(datasetRoot + 'dataset.json'))
-        print 'load ' + video_id
+        #データのロード
+        dataset = json.load(open(data_root + 'dataset.json'))
+        print('ロードした動画のID:', video_id)
+        #リストdatasetからvideo_idと一致する要素'videoID'を抽出(lambda x:無名関数)
         data = filter(lambda x: x['videoID'] == video_id, dataset)
         self.data = data[0]
-        self.feat = np.load(datasetRoot + 'feat/' + feat_type +
-                            '/' + video_id + '.npy').astype(np.float32)
-
-    def sampleFrame(self):
+        self.feat = np.load(data_root + 'feat/' + feat_type + '/' + video_id + '.npy').astype(np.float32)
+    
+    def SamplingFrame(self):
+        #frame rate
         fps = self.data['fps']
         fnum = self.data['fnum']
 
         idx = np.arange(fps, fnum, fps)
+        #np.floor():値の切り捨て
         idx = np.floor(idx)
+        #list化
         idx = idx.tolist()
-        idx = map(int, idx)
+        #idxをmap関数によりint型に変換
+        idx = map(idx, int)
 
         img = [self.data['image'][i] for i in idx]
         img_id = [self.data['imgID'][i] for i in idx]
