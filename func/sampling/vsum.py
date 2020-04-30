1c043a9ddfac2689096e2b2a6f7a9d4e8960f6d0
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-04-30 23:41:42 +0900

    Update vsum.py

--- func/sampling/vsum.py
+++ func/sampling/vsum.py
@@ -1,8 +1,9 @@
 #-*- coding:utf-8 -*-
+
+#モジュールのインポート#
 import gm_submodular
 import gm_submodular.example_objectives as ex
 from gm_submodular import leskovec_maximize
-
 from func.dataset.summe import SUMME
 import numpy as np
 import scipy.spatial.distance as dist
