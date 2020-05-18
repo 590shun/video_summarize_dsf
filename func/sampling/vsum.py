d805a4cdbedcb2ac0390d426b84929ec69e76a02
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-05-18 13:45:34 +0900

    Update vsum.py

--- func/sampling/vsum.py
+++ func/sampling/vsum.py
@@ -1,22 +1,24 @@
 #-*- coding:utf-8 -*-
 
 #モジュールのインポート#
-import gm_submoduler
-import gm_submoduler.example_objectives as ex
-from gm_submoduler import leskovec_maximize
-from func.dataset.summe import summe
+import gm_submodular
+import gm_submodular.example_objectives as ex
+from gm_submodular import leskovec_maximize
+from func.dataset.summe import SUMME
 import numpy as np
 import scipy.spatial.distance as dist
 
 #クラス定義#
-class VSUM(gm_submoduler.DataElement):
+class VSUM(gm_submodular.DataElement):
     def __init__(self, videoID, model, dataset='summe', featType='vgg', seg_l=5):
         
         #データセットの読み込み
-        self.dataset = summe(videoID)
+        #summe(self, video_id, feat_type='vgg')
+        self.dataset = SUMME(videoID)
         
         #生成する動画の長さ？(オリジナルの動画に対する割合)
         self.budget = int(0.15 * self.dataset.data['length'] / seg_l)
+        #生成する動画の長さの割合を表示
         print('budget:', self.budget)
 
         #動画のsegmentを埋め込む
@@ -32,6 +34,7 @@ class VSUM(gm_submoduler.DataElement):
 
         #時系列方向の距離を算出
         self.frame, img_id, self.score = self.dataset.sampleFrame()
+
         fno_arr = np.expand_dims(np.array(img_id), axis=1)
         self.dist_c = dist.pdist(fno_arr, 'sqeulidean')
     
@@ -61,31 +64,32 @@ class VSUM(gm_submoduler.DataElement):
             gt_score.append(self.score[i:i+seg_l])
         return selected, frames, gt_score
 
-    def encodeSeg(data, model, seg_size=5):
-        feat = data.feat
+def encodeSeg(data, model, seg_size=5):
+    feat = data.feat
 
-        img, img_id, score = data.sampleFrame()
-        segs = [img_id[i:i+seg_size] for i in range(len(img_id) - seg_size + 1)]
-        segs = reduce(lambda x, y: x + y, segs)
+    img, img_id, score = data.sampleFrame()
+    segs = [img_id[i:i + seg_size] for i in range(len(img_id) - seg_size + 1)]
+    segs = reduce(lambda x, y: x + y, segs)
 
-        x = feat[segs]
+    x = feat[segs]
 
-        #embedding
-        enc_x = model(x)
+    #embedding
+    enc_x = model(x)
         
-        return enc_x.data
-
-    def uniformity(S):
-        #入力:S(getChrDistance()のデータ要素)#
-        #return: uniformity objective#
-
-        tempDMat.mean()
-        return (lambda X: (1 - ex.kmedoid_loss(X, tempDMat, float(norm))))
+    return enc_x.data
     
-    def representativeness(S):
-        #入力: getDistances()のデータ要素#
-        #return representativeness objectiveness#
 
-        tempDMat = S.getDistances()
-        norm = tempDMat.mean()
-        return (lambda X: (1 - ex.kmedoid_loss(X, tempDMat, float(norm))))
+def uniformity(S):
+    #入力:S(getChrDistance()のデータ要素)#
+    #return: uniformity objective#
+
+    tempDMat.mean()
+    return (lambda X: (1 - ex.kmedoid_loss(X, tempDMat, float(norm))))
+    
+def representativeness(S):
+    #入力: getDistances()のデータ要素#
+    #return representativeness objectiveness#
+
+    tempDMat = S.getDistances()
+    norm = tempDMat.mean()
+    return (lambda X: (1 - ex.kmedoid_loss(X, tempDMat, float(norm))))
