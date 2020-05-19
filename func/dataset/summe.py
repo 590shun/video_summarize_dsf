0b5303d6450551bf2f8fe0987abff35f9aef3985
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-05-19 20:51:29 +0900

    Update summe.py

--- func/dataset/summe.py
+++ func/dataset/summe.py
@@ -7,20 +7,23 @@ import numpy as np
 import json
 
 #データセットが存在するフォルダの指定#
-data_root = './data/summe'
+data_root = './data/summe/'
 
 #クラスの定義#
-class summe():
+class SUMME():
     def __init__(self, video_id, feat_type='vgg'):
         #データのロード
-        dataset = json.load(open(data_root + 'dataset.json'))
+        #dataset:list型, 各要素はdict型
+        dataset = json.load(open(data_root + '/dataset.json'))
         print('ロードした動画のID:', video_id)
-        #リストdatasetからvideo_idと一致する要素'videoID'を抽出(lambda x:無名関数)
-        data = filter(lambda x: x['videoID'] == video_id, dataset)
+        #datasetからvideo_idと一致する要素'videoID'を抽出
+        data = list(filter(lambda x: x['videoID'] == video_id, dataset))
+        #print(type(data[0]))
+        #print(data[0].keys())
         self.data = data[0]
         self.feat = np.load(data_root + 'feat/' + feat_type + '/' + video_id + '.npy').astype(np.float32)
     
-    def SamplingFrame(self):
+    def sampleFrame(self):
         #frame rate
         fps = self.data['fps']
         fnum = self.data['fnum']
@@ -31,10 +34,13 @@ class summe():
         #list化
         idx = idx.tolist()
         #idxをmap関数によりint型に変換
-        idx = map(idx, int)
+        idx = list(map(int, idx))
 
         img = [self.data['image'][i] for i in idx]
         img_id = [self.data['imgID'][i] for i in idx]
         score = [self.data['score'][i] for i in idx]
+        #print('img:', img)
+        #print('img_id:', img_id)
+        #print('score:', score)
 
         return img, img_id, score
