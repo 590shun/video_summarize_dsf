3e8156c5d8badb66737deb107f7c9c366b41d3e0
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-05-19 20:53:03 +0900

    Update vid_enc.py

--- func/nets/vid_enc.py
+++ func/nets/vid_enc.py
@@ -2,6 +2,7 @@
 
 #モジュールのインポート#
 import sys, os
+import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,12 +16,13 @@ class Model(nn.Module):
         self.batch_size = batch_size
         super(Model, self).__init__()
         self.fc1 = nn.Linear(4096, 1000)
-        self.fc2 = nn.Liner(1000, 300)
+        self.fc2 = nn.Linear(1000, 300)
 
     def __call__(self, x_seg):
-        batch_size = self.batch_size['video']
-        with cuda.get_device(x_seg.data):
-            y0 = F.tanh(self.fc1(x_seg))
-            y1 = F.tanh(self.fc2(y0))
-            h = F.reshape(y1, (y1.shape[0] / batch_size, batch_size, 300))
-            return F.sum(h, axis=1) / batch_size
+        batch_size = self.batch_size['video'] #5
+        y0 = F.tanh(self.fc1(x_seg)) #torch.Size([510, 1000])
+        y1 = F.tanh(self.fc2(y0)) #torch.Size([510, 300])
+        h = torch.reshape(y1, (int(y1.shape[0] / batch_size), batch_size, 300))
+        return torch.sum(h, axis=1) / batch_size
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
