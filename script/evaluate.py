20e4491795424111dcfe9e05080f72d07e03ab8a
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2020-05-22 13:12:39 +0900

    Update evaluate.py

--- script/evaluate.py
+++ script/evaluate.py
@@ -1,11 +1,12 @@
 #-*- coding:utf-8 -*-
 
+##summarizationの評価実装##
+
 #モジュールのインポート#
 from scipy.io import loadmat
 import numpy as np
 import json
 from sklearn.metrics import f1_score
-
 
 header = '''|                         | Min. HUMAN | Avg. HUMAN | Max. HUMAN |  Summarization  |
 |------------------------:|:----------:|:----------:|:----------:|:---------------:|'''
@@ -13,26 +14,49 @@ header = '''|                         | Min. HUMAN | Avg. HUMAN | Max. HUMAN |  
 entry = '|{:>25}|{:^12.3}|{:^12.3}|{:^12.3}|{:^17.3}|'
 
 res_sum_mean = '|                     mF1 |{:^12.3}|{:^12.3}|{:^12.3}|{:^17.3}|'
-res_sum_rel_to_avr = '|   relative to avg.human |{:^12.4}|{:^12.4}|{:^12.4}|{:^17.4}|'
+res_sum_rel_to_avg = '|   relative to avg.human |{:^12.4}|{:^12.4}|{:^12.4}|{:^17.4}|'
 res_sum_rel_to_max = '|   relative to best.human|{:^12.4}|{:^12.4}|{:^12.4}|{:^17.4}|'
 
 #f1 scoreの算出#
 def eval_f1(pred_summary, gt_summary):
     pred_summary = (pred_summary > 0).astype(np.int)
     gt_summary = (gt_summary > 0).astype(np.int)
-    f1 = map(lambda y_true: f1_score(y_true, pred_summary), gt_summary)
+    f1 = list(map(lambda y_true: f1_score(y_true, pred_summary), gt_summary))
+    #print(f1)
     return sum(f1) / len(f1)
 
-#summaryの評価#
 def eval_summary(dataset_name, res_base, gt_base):
     res_base = res_base
 
+    data = json.load(open('data/{}/dataset.json'.format(dataset_name)))
+
+    res = {}
+    for d in data:
+        v_id = d['videoID']
+        pred_summary = np.load(res_base + '%s.npy' % v_id)
+
+        #print('gt_base:', gt_base)
+
+        gt_data = loadmat(gt_base + '/' + v_id + '.mat')
+        user_score = gt_data.get('user_score')
+        user_score = user_score.T
+
+        f1 = eval_f1(pred_summary, user_score)
+        res[v_id] = f1
+
+    return res
+
+#summaryの評価#
+def eval_human_summary(dataset_name, gt_base):
+    res_base = gt_base
+
     #データセットのロード
     data = json.load(open('data/{}/dataset.json'.format(dataset_name)))
-    res = {}
+    res_min, res_avg, res_max = {}, {}, {}
 
     for d in data:
-        gt_data = loadmat(gt_base +'/%s.mat' % v_id)
+        v_id = d['videoID']
+        gt_data = loadmat(gt_base + '/' + v_id + '.mat')
         #人間の手によるスコアを読み込む
         user_score = gt_data.get('user_score')
         user_score = user_score.T
@@ -50,3 +74,35 @@ def eval_summary(dataset_name, res_base, gt_base):
         res_max[v_id] = max(fscore_all) #最大
     
     return res_min, res_avg, res_max
+
+if __name__ == '__main__':
+    import os
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('resbase', type=str, default='', help='path to resulting .npy files')
+    parser.add_argument('--dname', '-d', type=str, default='summe', help='dataset name(summe)')
+    args = parser.parse_args()
+
+    dataset_name = args.dname
+    if dataset_name == 'summe':
+        gt_base = './data/summe/GT'
+    else:
+        raise NotImplementedError
+
+    res_base = args.resbase if args.resbase[-1] == os.sep else args.resbase + os.sep
+    res = eval_summary(dataset_name, res_base, gt_base)
+    hum_min, hum_avg, hum_max = eval_human_summary(dataset_name, gt_base)
+    
+    print('**********---------------------------- Result ----------------------------**********')
+    print(header)
+    
+    for k in res:
+        print(entry.format(k, hum_min[k], hum_avg[k], hum_max[k], res[k]))
+    
+    score_mean = [sum(r.values()) /len(r) for r in [hum_min, hum_avg, hum_max, res]]
+
+
+    print('|' + '|' * 82 +'|')
+    print(res_sum_mean.format(*score_mean))
+    print(res_sum_rel_to_avg.format(*map(lambda x: x / score_mean[1], score_mean)))
+    print(res_sum_rel_to_max.format(*map(lambda x: x / score_mean[2], score_mean)))
